2acadc76be2b5deb849e4fad5262817076b63719
Awais Ul Hassan <awaisul519@gmail.com>
2021-04-07 17:42:02 +0500

    PRODUCTS MANAGEMENT - PROJECT SET UP - LOGIN with mySql - SIGN UP with mySql

--- src/main/java/com/example/ProductsManager/HelloServlet.java
+++ src/main/java/com/example/ProductsManager/HelloServlet.java
@@ -1,27 +1,82 @@
 package com.example.ProductsManager;
 
 import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import com.example.ProductsManager.DatabaseConnection;
+
+import javax.xml.crypto.Data;
+
+
+import java.sql.SQLException;
+
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+private String message;
 
-    public void init() {
-        message = "Products Managment Servlet!";
+
+public void init() {
+message = "Products Management Servlet!";
+}
+
+public void doPost(HttpServletRequest request, HttpServletResponse response) throws  ServletException,IOException {
+
+try {
+
+response.setContentType("text/html");
+PrintWriter out = response.getWriter();
+
+    String n=request.getParameter("un");
+    String p=request.getParameter("up");
+    if(validate(n,p))
+    {
+        out.println("LOGIN SUCCESSFULL");
+        RequestDispatcher rd=request.getRequestDispatcher("/home.jsp");
+        rd.include(request,response);
+
+    }
+    else
+    {
+        out.println("LOGIN FAILED!!! WRONG USERNAME OR PASSWORD.");
+        RequestDispatcher rd=request.getRequestDispatcher("/index.jsp");
+        rd.include(request,response);
+
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
 
-    public void destroy() {
-    }
+out.println("</body></html>");
+}
+catch (Exception e){
+    e.printStackTrace();
+}
+}
+
+public static boolean validate(String name,String pass){
+boolean status=false;
+try{
+    Connection con = DatabaseConnection.initializeDatabase();
+
+    PreparedStatement ps=con.prepareStatement(
+            "select * from User where UserName=? and Password=?");
+    ps.setString(1,name);
+    ps.setString(2,pass);
+    ResultSet rs=ps.executeQuery();
+    status = rs.next();
+
+}
+catch(Exception e){
+    System.out.println(e);
+}
+
+return status;
+}
+
+
+
+public void destroy() {
+}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,12 +2,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>JSP - Hello World</title>
+<title>Managing Products</title>
 </head>
 <body>
-<h1 style="align-content: center"><%= "Products Manager!" %>
+<h1 align="center"><%= "-Products Management-" %>
 </h1>
+<div class="container" align="center">
+<h2>User Login</h2>
 <br/>
-<a href="hello-servlet">Hello Servlet</a>
+
+<form action="hello-servlet" method="post">
+   <label>Username: </label>
+   <input type="text" name="un" placeholder="Enter Your USERNAME">
+   <br>
+   <label>Password: </label>
+   <input type="text" name="up" placeholder="Enter Your PASSWORD">
+   <br>
+<input type="submit" value="login">
+
+   <a href="signUp.jsp">  Register</a>
+
+</form>
+<br>
+
+</div>
+
 </body>
 </html>
